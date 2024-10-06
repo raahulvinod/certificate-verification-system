e857136ae0b9a091307713a1ad965ddbaaf5e550
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-10-06 16:01:49 +0530

    fix: role based login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
 
 import User from '../model/user.model.js';
 
@@ -11,14 +10,46 @@ const generateToken = (id) => {
 
 export const signup = async (req, res) => {
   const { fullname, email, password, mobile } = req.body;
+
   try {
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+    const emailExists = await User.findOne({ email });
+    const mobileExists = await User.findOne({ mobile });
+
+    if (emailExists) {
+      return res
+        .status(400)
+        .json({ message: 'User already exists with this email.' });
+    } else if (mobileExists) {
+      return res
+        .status(400)
+        .json({ message: 'User already exists with this mobile number.' });
     }
+
     const user = await User.create({ fullname, email, password, mobile });
     const token = generateToken(user._id);
     res.status(201).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    if (user.role !== 'admin') {
+      return res
+        .status(403)
+        .json({ message: 'You are not authorized to login' });
+    }
+
+    const token = generateToken(user._id);
+    res.json({ token, user });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
